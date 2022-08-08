a55418774ebb080b02ac9ca30bc241fb2a57117b
Ilana <ilana@Justins-Macbook.lan>
2022-08-08 18:06:03 -0500

    resolving studio-3 issues

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipmentArray: object[] = [
+    {item: "Habitat dome"},
+    {item: "Drones"},
+    {item: "Food containers"},
+    {item: "Oxygen tanks"}
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
-
+//   add(equipmentName: string: boolean) {
+//     this.equipment.push({name: memberName, firstMission: isFirst});
+//   }
+//   remove(member: object) {
+//     let index = this.crew.indexOf(member);
+//     this.crew.splice(index, 1);
+//   }
+//   memberBeingEdited: object = null;
+//   edit(member: object) {
+//     this.memberBeingEdited = member;
+//  }
+//  save(name: string, member: object) {
+//   member['name'] = name;
+//   this.memberBeingEdited = null;
+//   }
 }
